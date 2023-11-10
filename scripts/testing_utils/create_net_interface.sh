684b360b94d9b234a98a1b48192e393b83a09f2b
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2023-11-10 09:53:56 +0530

    Updating files
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/testing_utils/create_net_interface.sh
+++ scripts/testing_utils/create_net_interface.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-INT_NAME=${INTERFACE_NAME:="ocp"}
+export INT_NAME=${INTERFACE_NAME:="ocp"}
+export IPV4_INT_ADDRESS=${IPV4_INT_ADDRESS?=please provide ipv4 interface address}
+export IPV4_START=${IPV4_START?=please provide starting ipv4 address}
+export IPV4_END="${IPV4_END?=please provide ending ipv4 address}"
+export IPV4_NETMASK="${IPV4_NETMASK?=please provide ipv4 net mask}"
+export IPV6_INT_ADDRESS=${IPV6_INT_ADDRESS?=please provide ipv6 interface address}
+export IPV6_PREFIX=${IPV6_PREFIX?=please provide ipv6 prefix}
+export BR_NAME="br-${INT_NAME}"
+export DUMMY_NAME="dummy-${INT_NAME}"
 
-cat > /etc/sysconfig/network-scripts/ifcfg-br-${INT_NAME} <<EOF
-DEVICE=br-${INT_NAME}
+cat > /etc/sysconfig/network-scripts/ifcfg-${BR_NAME} <<EOF
+DEVICE=${BR_NAME}
 TYPE=Bridge
 ONBOOT=yes
 BOOTPROTO=none
@@ -12,9 +20,25 @@ DELAY=0
 EOF
 
 systemctl restart NetworkManager
-ip link add name br-${INT_NAME} type bridge || true
-ip link add dummy-${INT_NAME} type dummy
-ip link set dev br-${INT_NAME} up
-ip link set dev dummy-{INT_NAME} master br-${INT_NAME}
+ip link add name ${BR_NAME} type bridge || true
+ip link add ${DUMMY_NAME} type dummy
+ip link set dev ${BR_NAME} up
+ip link set dev {DUMMY_NAME} master ${BR_NAME}
 
-
+cat > ${INT_NAME}.xml <<EOF
+<network>
+  <name>${INT_NAME}</name>
+  <forward mode='nat'>
+    <nat>
+      <port start='1024' end='65535'/>
+    </nat>
+  </forward>
+  <bridge name='${BR_NAME}' stp='on' delay='0'/>
+  <ip address='${IPV4_INT_ADDRESS}' netmask='${IPV4_NETMASK}'>
+    <dhcp>
+      <range start='${IPV4_START}' end='${IPV4_END}'/>
+    </dhcp>
+  </ip>
+  <ip family="ipv6" address="${IPV6_INT_ADDRESS}" prefix="${IPV6_PREFIX}"
+</network>
+EOF
